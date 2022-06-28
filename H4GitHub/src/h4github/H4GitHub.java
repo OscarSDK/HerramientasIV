3df1d5f0c3664258f374432bd244eabdfd1f08ca
user <user@DESKTOP-EOQAJOP>
2022-06-28 08:16:28 -0500

    Segunda Subida del proyecto

--- H4GitHub/src/h4github/H4GitHub.java
+++ H4GitHub/src/h4github/H4GitHub.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package h4github;
 
-/**
- *
- * @author user
- */
+
 public class H4GitHub {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        int x = 4;
+        int y = 5;
+        int suma = x+y;
+        System.out.println("La suma:"+suma);
     }
     
 }
